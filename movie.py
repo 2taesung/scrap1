ee9d72cc89478d7e02780f23977dc197c0a67611
kim-mg <audrjs3535@naver.com>
2020-08-06 21:20:34 +0900

    movie.py 수정함, 실행 사진 포함

--- movie.py
+++ movie.py
@@ -14,6 +14,7 @@ soup = BeautifulSoup(response.text, 'html.parser')
 movie_section = soup.select(
     '#content > div.article > div:nth-child(1) > div.lst_wrap > ul > li' )
 
+movie_datas = []
 for movie in movie_section:
     title_tag = movie.select_one('div > a > img')
     code_tag = movie.select_one('div > a')
@@ -25,12 +26,47 @@ for movie in movie_section:
     # print(movie_title)
     # print(movie_code_list[1], '\n')
 
-    news_data = {
+    movie_data = {
         'title' : movie_title,
         'code' : movie_code_list[1]
     }
+    movie_datas.append(movie_data)
+    # with open('./movies.csv', 'a') as csvfile:
+    #     fieldnames = ['title', 'code']
+    #     csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    #     csvwriter.writerow(movie_data)
 
-    with open('./movies.csv', 'a') as csvfile:
-        fieldnames = ['title', 'code']
-        csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        csvwriter.writerow(news_data)
+# print(movie_datas)
+
+for movie in movie_datas:
+    movie_code = movie['code']
+
+    REVIEW_URL = r'https://movie.naver.com/movie/bi/mi/basic.nhn?code={movie_code}'
+
+    params = (
+        ('code', movie_code),
+        ('type', 'after'),
+        ('isActualPointWriteExecute', 'false'),
+        ('isMileageSubscriptionAlready', 'false'),
+        ('isMileageSubscriptionReject', 'false'),
+    )
+
+    response = requests.get('https://movie.naver.com/movie/bi/mi/pointWriteFormList.nhn', params=params)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    review_section = soup.select(
+        'body > div > div > div.score_result > ul > li' )
+    
+    count = 0
+    for review in review_section:
+        score = review.select_one('.star_score > em').text
+
+        if review.select_one(f'.score_reple > p > span#_filtered_ment_{count} > span#_unfold_ment{count}') is None:
+            review = review.select_one(f'.score_reple > p > span#_filtered_ment_{count}').text.strip()
+        elif review.select_one(f'.score_reple > p > span#_filtered_ment_{count} > span#_unfold_ment{count}'):
+            review = review.select_one(f'.score_reple > p > span#_filtered_ment_{count} > span > a')['data-src']
+
+
+        print("평점 : ", score, "\n리뷰 : ", review, "\n")
+
+        count += 1
